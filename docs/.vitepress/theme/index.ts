805d1eceaa6db9a3b56332945997c2059a12a304
woeoio <woeoio@qq.com>
2025-10-19 14:45:08 +0800

    +引入图片查看器插件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,12 +4,25 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
+
+/**************插件*************** */
+
+// 引入图片查看器
+import imageViewer from "vitepress-plugin-image-viewer";
+import "viewerjs/dist/viewer.min.css";
+
+import { useRoute } from 'vitepress'
+
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
+  },
+  setup() {
+    // 使用图片查看器
+    imageViewer(useRoute());
   },
   enhanceApp({ app, router, siteData }) {
     // ...
